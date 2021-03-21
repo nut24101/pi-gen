b1616b46777ae329a5e358ef23073098fa2564f5
Nut Charoenpattanasirikul <nut.c@apipluspower.com>
2021-03-21 19:52:37 +0700

    Update engine

--- stage2/04-install-flutter/01-run.sh
+++ stage2/04-install-flutter/01-run.sh
@@ -1,4 +1,5 @@
-install -m 644 ./flutter-pi-img-gen/engine-binaries/arm/libflutter_engine.so.* /flutter-pi-img-gen/engine-binaries/arm/icudtl.dat "${ROOTFS_DIR}/usr/lib/"
-install -m 644 ./flutter-pi-img-gen/engine-binaries/flutter_embedder.h "${ROOTFS_DIR}/usr/include"
+install -m 644 ./flutter-pi-img-gen/flutter-engine-binaries-for-arm/arm/libflutter_engine.so.* /flutter-pi-img-gen/engine-binaries/arm/icudtl.dat "${ROOTFS_DIR}/usr/lib/"
+install -m 644 ./flutter-pi-img-gen/flutter-engine-binaries-for-arm/flutter_embedder.h "${ROOTFS_DIR}/usr/include"
+
 
 cp -r ./flutter-pi-img-gen/flutter-pi "${ROOTFS_DIR}/home/pi"
