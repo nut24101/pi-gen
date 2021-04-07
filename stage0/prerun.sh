a619fa45dd164853a6d7118d1f4a513d4736389e
Nut <nut.c@apipluspower.com>
2021-04-07 11:19:01 +0700

    change mirror

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
 
 if [ ! -d "${ROOTFS_DIR}" ]; then
-	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
-fi
+	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://mirror.kku.ac.th/raspbian/raspbian/
