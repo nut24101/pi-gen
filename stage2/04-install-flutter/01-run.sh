6eeae1ddccf892149f3bb09112fe93f6fddbaa5f
Nut <nut.c@apipluspower.com>
2021-04-07 14:09:16 +0700

    fix code

--- stage2/04-install-flutter/01-run.sh
+++ stage2/04-install-flutter/01-run.sh
@@ -1,4 +1,4 @@
-sudo ./flutter-pi-img-gen/flutter-install.sh
+
 #install -m 644 ./flutter-pi-img-gen/flutter-engine-binaries-for-arm/arm/libflutter_engine.so.* /flutter-pi-img-gen/flutter-engine-binaries-for-arm/arm/icudtl.dat "${ROOTFS_DIR}/usr/lib/"
 #install -m 644 ./flutter-pi-img-gen/flutter-engine-binaries-for-arm/flutter_embedder.h "${ROOTFS_DIR}/usr/include"
 
