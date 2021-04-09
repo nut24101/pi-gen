aba195c03149b161c073c94f320e761c1078a20b
Nut <nut.c@apipluspower.com>
2021-04-09 13:08:59 +0700

    fix clone engine

--- stage2/04-install-flutter/01-run-chroot.sh
+++ stage2/04-install-flutter/01-run-chroot.sh
@@ -1,9 +1,10 @@
-git clone --depth 1 https://github.com/ardera/flutter-engine-binaries-for-arm.git home/pi/flutter-pi-img-gen/engine-binaries
+[ ! -d "home/pi/flutter-pi-img-gen/engine-binaries" ] && git clone --depth 1 https://github.com/ardera/flutter-engine-binaries-for-arm.git home/pi/flutter-pi-img-gen/engine-binaries
+
 cd home/pi/flutter-pi-img-gen/engine-binaries
 ./install.sh
 cd /
 
-git clone https://github.com/ardera/flutter-pi home/pi/flutter-pi-img-gen/flutter-pi 
+[ ! -d "home/pi/flutter-pi-img-gen/flutter-pi " ] && git clone https://github.com/ardera/flutter-pi home/pi/flutter-pi-img-gen/flutter-pi 
 cmake -B /home/pi/flutter-pi-img-gen/flutter-pi/build -S /home/pi/flutter-pi-img-gen/flutter-pi
 make -j `nproc` -C /home/pi/flutter-pi-img-gen/flutter-pi/build
 make install -C /home/pi/flutter-pi-img-gen/flutter-pi/build
@@ -12,4 +13,7 @@ cd /media && mkdir usb
 cd /home/admin && mkdir flutter-apps && cd flutter-apps && mkdir soilcrete
 
 printf "\n[pi4]\ndtoverlay=vc4-fkms-v3d\nmax_framebuffers=2\n\n[all]\ndtoverlay=vc4-fkms-v3d\n" | tee -a /boot/config.txt
+
+echo "cd /home/admin/flutter-apps/soilcrete && flutter-pi flutter_assets" | tee -a /home/admin/.bashrc
+
 usermod -a -G render admin
